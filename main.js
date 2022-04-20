1584a31b7684353a500cc5bd9e20f8c8406a3af4
Tomek Zawadzki <tomekzawadzki1990@gmail.com>
2022-04-20 23:20:26 +0200

    add file with JS and file with normalizaton, few small changes in style and html

---
+++ main.js
@@ -0,0 +1,6 @@
+console.log("Hello Tomcio");
+
+const coachText = document.querySelector('.coach__carleto--text-js');
+console.log(coachText);
+
+coachText.innerHTML ="Włoski trener i piłkarz z wieloma sukcesami na koncie";
