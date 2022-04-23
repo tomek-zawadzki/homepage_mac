1ba43fe5156bfdc6df3e8dbb35aa60b730eb5555
Tomek Zawadzki <tomekzawadzki1990@gmail.com>
2022-04-23 16:34:48 +0200

    practise function JS'
    ok

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 console.log("Hello Tomcio");
 
-const coachText = document.querySelector('.coach__carleto--text-js');
+const coachText = document.querySelector(".coach__carleto--text-js");
 console.log(coachText);
 
-coachText.innerHTML ="Włoski trener i piłkarz z wieloma sukcesami na koncie";
+coachText.innerHTML = "Włoski trener i piłkarz z wieloma sukcesami na koncie";
+/*
+const visitor = prompt("Jak masz na imię przyjacielu?");
+
+alert(`Witaj ${visitor} poglądaj sobie troszeczkę`);
+*/
+
+function createContent(querySelectorContent, content) {
+  const element = document.querySelector(querySelectorContent);
+  element.innerHTML = content;
+}
+
+createContent(`.special-js`, `${visitor}`);
